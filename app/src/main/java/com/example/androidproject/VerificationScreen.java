80c4a9a519479fbecdef2562bee626f10baf7e27
Siva Prakash <prakashdeveloper@outlook.in>
2023-06-28 19:27:08 +0530

    Added otp screen

--- app/src/main/java/com/example/androidproject/VerificationScreen.java
+++ app/src/main/java/com/example/androidproject/VerificationScreen.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class MainScreen extends Fragment {
+public class VerificationScreen extends Fragment {
 
     @Override
     public View onCreateView(
@@ -17,6 +17,6 @@ public class MainScreen extends Fragment {
             ViewGroup container,
             Bundle savedInstanceState
     ) {
-        return inflater.inflate(R.layout.fragment_main_screen, container, false);
+        return inflater.inflate(R.layout.fragment_verification_screen, container, false);
     }
 }
