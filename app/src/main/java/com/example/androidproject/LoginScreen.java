e23aca1e455a66b11f301878a31cf704fef38409
Sanjay <sanjaymsp2001@gmail.com>
2023-06-28 17:54:29 +0530

    setup basic requirements

---
+++ app/src/main/java/com/example/androidproject/LoginScreen.java
@@ -0,0 +1,37 @@
+package com.example.androidproject;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.androidproject.databinding.FragmentLoginScreenBinding;
+
+
+public class LoginScreen extends Fragment {
+    private FragmentLoginScreenBinding binding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = FragmentLoginScreenBinding.inflate(getLayoutInflater());
+
+        var host = NavHostFragment.findNavController(this);
+        binding.button.setOnClickListener(v -> host.navigate(R.id.mainScreen));
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
+}
